10bca40d7188d4ef4e759074214dd899941f54c6
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-06-29 14:39:34 -0300

    readme
    readme

--- estudo/src/condicionais/condicionais/MediaIdade.java
+++ estudo/src/condicionais/condicionais/MediaIdade.java
@@ -6,26 +6,30 @@ public class MediaIdade {
     public static void main(String[] args) {
         Scanner tec = new Scanner(System .in);
 
-        int cont = 0;
-        int media, idade;
+        float idade, media;
+        float cont = 0;
+        float soma = 0;
+      
 
         System.out.println("Digite as idades ");
         idade = tec.nextInt();
 
-while (idade < 0) {
-    if (idade < 0 ){
+        //Lógica While
+    while (idade >= 0) {
+
+    cont = cont + 1;
+    soma = soma + idade;
+    
+    idade = tec.nextInt();
+
+    }
+    //Lógica if else
+    if (cont == 0 ){
         System.out.println("IMPOSSIVEL CALCULAR");
     }else {
-        
-        cont = cont + 1;
-        idade = idade + idade;
-        media = idade / cont;
-
-
+        media = soma / cont;
+        System.out.printf("Média %.2f", media);
     }
 
-    
 }
-
-    }
 }
