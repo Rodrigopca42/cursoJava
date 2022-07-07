5caa6772693f02790c30d6be7c6e5dcf53be22d7
Rodrigo Cardoso <rodrigopca426281@gmail.com>
2022-07-07 14:45:53 -0300

    readme
    readme

--- estudo/src/data/Comemoracao.java
+++ estudo/src/data/Comemoracao.java
@@ -3,38 +3,38 @@ public class Comemoracao {
 
     public static void main(String[] args) {
         
-        Calendário viviane = new Calendário();
-        viviane.dia = 31;
-        viviane.mes = 03;
-        viviane.ano = 1980;
+        Calendário v1 = new Calendário();
+        v1.dia = 31;
+        v1.mes = 03;
+        v1.ano = 1980;
 
-        Calendário daniel = new Calendário();
-        daniel.dia = 25;
-        daniel.mes = 03;
-        daniel.ano = 2012;
+        Calendário d1 = new Calendário();
+        d1.dia = 25;
+        d1.mes = 03;
+        d1.ano = 2012;
 
-        Calendário millena = new Calendário();
-        millena.dia = 19;
-        millena.mes = 10;
-        millena.ano = 2010;
+        Calendário m1 = new Calendário();
+        m1.dia = 19;
+        m1.mes = 10;
+        m1.ano = 2010;
 
-        Calendário rodrigo = new Calendário();
-        rodrigo.dia = 11;
-        rodrigo.mes = 05;
-        rodrigo.ano = 1979;
+        Calendário r1 = new Calendário();
+        r1.dia = 11;
+        r1.mes = 05;
+        r1.ano = 1979;
 
         
         
-        System.out.printf("A Viviane nasceu no dia %d / %d de %d",viviane.dia, viviane.mes, viviane.ano);
+        System.out.printf("A Viviane nasceu no dia %d / %d de %d",v1.dia,v1.mes,v1.ano);
         System.out.println("");
 
-        System.out.printf("O Daniel nasceu no dia %d / %d de %d",daniel.dia, daniel.mes, daniel.ano);
+        System.out.printf("O Daniel nasceu no dia %d / %d de %d",d1.dia, d1.mes, d1.ano);
         System.out.println("");
 
-        System.out.printf("A Millena nasceu no dia %d / %d de %d",millena.dia, millena.mes, millena.ano);
+        System.out.printf("A Millena nasceu no dia %d / %d de %d",m1.dia, m1.mes, m1.ano);
         System.out.println("");
 
-        System.out.printf("O Rodrigo nasceu no dia %d / %d de %d",rodrigo.dia, rodrigo.mes, rodrigo.ano);
+        System.out.printf("O Rodrigo nasceu no dia %d / %d de %d",r1.dia, r1.mes, r1.ano);
         System.out.println("");
     }
 }
